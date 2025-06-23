0a8a48f844f0e6fad857ab6e59d93b5c192fbb53
Jake <jake@megster.net>
2025-06-23 14:22:27 +0100

    Setup editorconfig

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { ButtonProps } from './types';
+import React from "react";
+import { ButtonProps } from "./types";
 
-import styles from './Button.module.scss';
+import styles from "./Button.module.scss";
 
 const Button: React.FC<ButtonProps> = ({ label }) => {
-  return <button className={styles.btn}>{label}</button>;
+    return <button className={styles.btn}>{label}</button>;
 };
 
 export default Button;
